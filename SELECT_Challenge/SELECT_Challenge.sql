fa4199983c395f83b501825be3d13f032aab522e
Vick <vlopez0392@gmail.com>
2020-05-08 00:00:44 +0800

    SELECT challange, SELECT DISTINCT and Challenge

---
+++ SELECT_Challenge/SELECT_Challenge.sql
@@ -0,0 +1,10 @@
+-- SELECT CHALLENGE 
+
+-- Business Situation: We want to send out a promotional email to our existing customers. 
+
+/* Challenge Question: Use the SELECT statement to grab the first and last names of every
+ * customer and their email addresses */ 
+
+-- Solution: (Query coincides with proposed solution) 
+
+SELECT first_name, last_name, email FROM customer -- May not be displayed in the same order as video.
